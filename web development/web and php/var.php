f7ec8f25a4b12d42b8b8341d11295e9b118d6e28
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2024-03-29 18:56:51 +0530

    Add files via upload

---
+++ web development/web and php/var.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+    <body>
+        <?php
+        $s=1;
+        $a="sonu";
+        echo $s;
+        echo "<br>";
+        echo $a;
+        ?>
+        </body>
+        </html>
